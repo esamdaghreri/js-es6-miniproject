9e2ccba34b60861e48a08dbbd19a722fe1635adb
esamdaghreri <esam@daghreri.com>
2020-06-03 21:41:55 +0300

    final project

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,149 @@
-// Global app controller
-import num from './test';
-const variabl = 23;
-console.log(`${num} ${variabl}`);
+import Search from './models/Search';
+import Recipe from './models/Recipe';
+import List from './models/List';
+import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
+import { elements, renderLoader, clearLoader } from './views/base';
+
+/** Global state of the app
+ * - Search object
+ * - Currect recipe object
+ * - Shoppinglist object
+ * - Liked recipes
+ */
+const state = {}
+window.state = state;
+/**
+ *  Search Controller
+ */
+const controlSearch = async () => {
+    // 1) Get query form view
+    const query = searchView.getInput();
+
+    if(query) {
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for result
+        searchView.clearInput();
+        searchView.clearResult();
+        renderLoader(elements.searchRes);
+        try{
+            //4) Search for recipes
+            await state.search.getResult();
+    
+            // 5) Render result on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        }catch (error) {
+            console.log(error);
+            alert('Something went wrong :( ')
+            clearLoader();
+        }
+    }
+
+
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if(btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResult();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
+
+
+/**
+ *  Recipe Controller
+ */
+const controlRecipe = async() => {
+    // Get ID from url
+    const id = window.location.hash.replace('#', '');
+    if(id) {
+        // Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+
+        // Highlisht selected search item
+        if (state.search) searchView.highlighSelected(id);
+
+        // Crete new recipe object
+        state.recipe = new Recipe(id);
+
+        try{
+            // Get recipe data and parse ingredients
+            await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
+
+            // Calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            // Render recipe
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
+
+        }catch (error) {
+            console.log(error);
+            alert('Something went wrong :( ')
+        }
+
+    }
+}
+['hashchange', 'load'].forEach(e => window.addEventListener(e, controlRecipe));
+/**
+ *  List Controller
+ */
+const controlList = () => {
+    // Create  a new list if there in none yet
+    if (!state.list) state.list = new List();
+
+    // Add each ingredient to the list and UI
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    });
+}
+// Handling delete and update list item events
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+
+    // Handle the delete button
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+        // Delete from state
+        state.list.deleteItem(id);
+        // Delete from view
+        listView.deleteItem(id);
+    // Handel the count update
+    } else if(e.target.matches('shopping___count-value')){
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateCount(id, val);
+    }
+});
+
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        // Decrease button is clicked
+        if(state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        // Increase button is clicked
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    } else if(e.target.matches('.recipe__btn--add , .recipe__btn--add *')){
+        controlList();
+    }
+});
+
+window.l = new List(); 
